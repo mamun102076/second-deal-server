bd3cb42acbbfd27b89e6a7b3be18b54b4f0906f5
mamun102076 <mamun102076@gmail.com>
2023-01-08 02:57:05 +0600

    implemented jwt token

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const port  = process.env.PORT || 5000
+const jwt = require('jsonwebtoken')
+const port = process.env.PORT || 5000
 require('dotenv').config()
 
 app.use(express.json())
@@ -12,55 +13,111 @@ app.use(cors())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ampetqi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyJwt = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(404).send('unauthorized access')
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(404).send('unauthorized access')
+        }
+        req.decoded = decoded
+        next()
+    })
+}
+
 async function run() {
-    try{
+    try {
         const categoryCollections = client.db('secondDeal').collection('category')
         const bookingCollections = client.db('secondDeal').collection('booking')
         const productsCollections = client.db('secondDeal').collection('products')
         const sellerCollections = client.db('secondDeal').collection('seller')
         const buyersCollections = client.db('secondDeal').collection('buyer')
 
-        app.get('/category',async (req,res) => {
+        const verifyAdmin = async (req,res,next) => {
+            const decodedEmail = req.decoded.email
+            const query = { email: decodedEmail}
+            const buyerUser = await buyersCollections.findOne(query)
+            const sellerUser = await sellerCollections.findOne(query)
+            if (buyerUser.role!=="Admin" || sellerUser.role!=="Admin") {
+                return res.status(404).send('unauthorized access')
+            }
+            next()
+        }
+
+        app.get('/category', async (req, res) => {
             const query = {}
             const result = await categoryCollections.find(query).toArray()
             res.send(result)
         })
 
-        app.get('/category/:id',async (req,res) => {
+        app.get('/category/:id', async (req, res) => {
             const id = req.params.id
             const filter = { _id: ObjectId(id) }
-            const result = await categoryCollections.findOne(filter)  
+            const result = await categoryCollections.findOne(filter)
             res.send(result)
         })
 
-        app.get('/booking',async(req,res) => {
-            const query = {}
+        app.get('/booking', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
             const booking = await bookingCollections.find(query).toArray()
             res.send(booking)
         })
 
-        app.post('/booking',async(req,res) => {
-            const booking =  req.body
+        app.post('/booking', async (req, res) => {
+            const booking = req.body
             const result = await bookingCollections.insertOne(booking)
             res.send(result)
         })
 
-        app.get('/products',async(req,res) => {
-            const query =  {}
+        app.get('/products', async (req, res) => {
+            const query = {}
             const result = await productsCollections.find(query).toArray()
             res.send(result)
         })
 
-        app.post('/products',async(req,res) => {
-            const product =  req.body
+        app.post('/products', async (req, res) => {
+            const product = req.body
             const result = await productsCollections.insertOne(product)
             res.send(result)
         })
 
-        app.post('/users',async(req,res) => {
+        app.delete('/products/:id',verifyJwt, async (req, res) => {
+            const decodedEmail = req.decoded.email
+            const query = { email: decodedEmail }
+            const seller = await sellerCollections.findOne(query)
+            console.log(seller)
+            if (!seller) {
+                return res.status(404).send('unathorized access')
+            }
+
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const result = await productsCollections.deleteOne(filter)
+            res.send(result)
+        })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const buyer = await buyersCollections.findOne(query)
+            const seller = await sellerCollections.findOne(query)
+
+            if (buyer || seller) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token })
+            }
+            res.send({ accessToken: '' })
+        })
+
+
+        app.post('/users', async (req, res) => {
             const query = req.body
 
-            if (query.providerId=="seller") {
+            if (query.providerId == "seller") {
                 const seller = await sellerCollections.insertOne(query)
                 return res.send(seller)
             }
@@ -69,33 +126,33 @@ async function run() {
             res.send(buyer)
         })
 
-        app.get('/buyers',async (req,res) => {
+        app.get('/buyers', async (req, res) => {
             const query = {}
             const buyers = await buyersCollections.find(query).toArray()
             res.send(buyers)
         })
 
-        app.delete('/buyers/:id',async (req,res) => {
+        app.delete('/buyers/:id',verifyJwt, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const filter = { _id: ObjectId(id) }
             const buyer = await buyersCollections.deleteOne(filter)
             res.send(buyer)
         })
 
-        app.get('/seller',async (req,res) => {
+        app.get('/seller', async (req, res) => {
             const query = {}
             const sellers = await sellerCollections.find(query).toArray()
             res.send(sellers)
         })
 
-        app.delete('/seller/:id',async (req,res) => {
+        app.delete('/seller/:id',verifyJwt,verifyAdmin, async (req, res) => {
             const id = req.params.id
             const filter = { _id: ObjectId(id) }
             const seller = await sellerCollections.deleteOne(filter)
             res.send(seller)
         })
 
-        app.put('/users/admin/:id',async(req,res) => {
+        app.put('/users/admin/:id', verifyJwt, verifyAdmin, async (req, res) => {
             const id = req.params.id
             const filter = { _id: ObjectId(id) }
             const buyer = await buyersCollections.findOne(filter)
@@ -105,27 +162,24 @@ async function run() {
                 }
             }
             const options = { upsert: true }
-
             if (buyer) {
-                const buyerUpdate = await buyersCollections.updateOne(filter,updatedDoc,options)
+                const buyerUpdate = await buyersCollections.updateOne(filter, updatedDoc, options)
                 return res.send(buyerUpdate)
             }
-
-            const sellerUpdate = await sellerCollections.updateOne(filter,updatedDoc,options)
+            const sellerUpdate = await sellerCollections.updateOne(filter, updatedDoc, options)
             res.send(sellerUpdate)
-        })
-
+        })        
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.dir)
 
-app.get('/',async (req,res) => {
+app.get('/', async (req, res) => {
     res.send('Second deal is running')
 })
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`Second deal is running on port ${port}`)
 })
